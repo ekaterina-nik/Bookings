e1f5a9266dab09ef5757940a85cf198447083801
ekaterina-nik <116982242+ekaterina-nik@users.noreply.github.com>
2022-10-30 02:17:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pandas as pd
 bookings = pd.read_csv('C:/Users/Екатерина/Downloads/bookings.csv', sep=';')
+
 bookings.columns = bookings.columns.str.replace(' ', '_').str.lower()
+
+a = bookings.groupby('country').agg({'is_canceled':'sum'}).sort_values('is_canceled', ascending = False)
+print(a)
+
+b = bookings.groupby('hotel').agg({'stays_total_nights': 'mean'}).round(2)
+print(b)
+
+g = bookings.query("assigned_room_type != reserved_room_type")
+print(g)
+
 bookings['total_kids'] = bookings[['children', 'babies']].sum(axis=1)
 bookings['has_kids'] = bookings['total_kids'] > 0
 c = bookings.query('has_kids == False and is_canceled == 1').shape[0]/bookings.query('has_kids == False').shape[0]*100
